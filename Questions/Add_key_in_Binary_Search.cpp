4d7a6bc98cf4200a29a2acb9922d9bd8ea01d023
divyanshgoyal777 <tonisha24680@gmail.com>
2024-09-27 20:06:38 +0530

    update

---
+++ Questions/Add_key_in_Binary_Search.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void add(vector<int> &v, int key)
+{
+    int i = 0, j = v.size() - 1;
+    // while (i < j)
+    // {
+    //     int mid = i + (j - i) / 2;
+    //     if (v[mid] > key)
+    //     {
+    //         if (mid > 0 && v[mid - 1] < key)
+    //         {
+    //             v.insert(v.begin() + mid, key);
+    //         }
+    //         else
+    //         {
+    //             j = mid - 1;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         if (mid < v.size() - 1 && v[mid + 1] > key)
+    //         {
+    //             v.insert(v.begin() + mid + 1, key);
+    //         }
+    //         else
+    //         {
+    //             i = mid + 1;
+    //         }
+    //     }
+    // }
+
+    while (i < j)
+    {
+        int mid = i + (j - i) / 2;
+        if (v[mid] < key)
+        {
+            i = mid + 1;
+        }
+        else
+        {
+            j = mid;
+        }
+    }
+    v.insert(v.begin() + i, key);
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 4, 5, 6, 7, 8};
+    int key = 3;
+    add(v, key);
+    for (int value : v)
+    {
+        cout << value << " ";
+    }
+    return 0;
+}
